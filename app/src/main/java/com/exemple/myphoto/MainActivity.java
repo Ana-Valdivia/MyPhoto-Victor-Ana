9f16b08621babcd375fbfd02c4f17c8bb5898b4f
Ana-valdivia <ana.raquel1291@hotmail.com>
2019-09-16 22:02:53 -0300

    Visualizar MyPhoto

--- app/src/main/java/com/exemple/myphoto/MainActivity.java
+++ app/src/main/java/com/exemple/myphoto/MainActivity.java
@@ -51,9 +51,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void onvisualizar(View view){
+    public void onVisualizar(View view){
 
-
+       Intent intent = new Intent(Intent.ACTION_VIEW);
+       intent.setDataAndType(uri,"image/jpeg");
+       startActivity(intent);
     }
 }
 
